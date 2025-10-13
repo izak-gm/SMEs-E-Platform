91ad8b9b978d8f23212e752470c741173b47a049
izak <maingiisaac27@gmail.com>
2025-10-13 18:06:00 +0300

    Less security configurations deu to password exposed

--- bizhub/api/apps.py
+++ bizhub/api/apps.py
@@ -1,6 +1,17 @@
 from django.apps import AppConfig
+import py_eureka_client.eureka_client as eureka_client
 
 
 class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
+
+EUREKA_SERVER = "http://localhost:8761/eureka"
+APP_NAME = "django-microservice"
+INSTANCE_PORT = 8000
+
+eureka_client.init(
+    eureka_server=EUREKA_SERVER,
+    app_name=APP_NAME,
+    instance_port=INSTANCE_PORT
+)
