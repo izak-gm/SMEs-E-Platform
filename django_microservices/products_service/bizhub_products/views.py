83e2ffbacde2b1b3c9a99353b1187827d320c025
izak <maingiisaac27@gmail.com>
2025-10-21 21:08:10 +0300

    product service view minor changes

--- django_microservices/products_service/bizhub_products/views.py
+++ django_microservices/products_service/bizhub_products/views.py
@@ -1,39 +1,59 @@
 from rest_framework import viewsets, permissions
-from rest_framework.permissions import IsAuthenticated
-
-from .models import Product, Store, SellerKYC, Brand, Category, ProductVariant, ProductImage
-from .serializers import ProductSerializer, StoreKYCSerializer, BrandSerializer, CategorySerializer, ProductVariantSerializer,ProductImageSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from django_microservices.common.auth.authentication import JWTAuthentication
-from django_microservices.common.auth.role_based_permissions import IsBuyer,IsSeller
+from django_microservices.common.auth.role_based_permissions import IsBuyer, IsSeller, IsAdmin
 
-# Create your views here.
+from .models import (
+    Product, Store, SellerKYC, Brand, Category, ProductVariant, ProductImage
+)
+from .serializers import (
+    ProductSerializer, StoreKYCSerializer, BrandSerializer,
+    CategorySerializer, ProductVariantSerializer, ProductImageSerializer
+)
+
+# --- Store Views ---
 class StoreViewSet(viewsets.ModelViewSet):
     queryset = Store.objects.all()
     serializer_class = ProductSerializer
-
-    # TODO Add permissions for Authenticated and Authorized Store owner
-    authentication_classes = JWTAuthentication
-    permission_classes = (IsAuthenticated,IsSeller)
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated, IsSeller]
 
 class StoreKYCViewSet(viewsets.ModelViewSet):
     queryset = SellerKYC.objects.all()
     serializer_class = StoreKYCSerializer
 
+# --- Common Models ---
 class BrandViewSet(viewsets.ModelViewSet):
     queryset = Brand.objects.all()
     serializer_class = BrandSerializer
+
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
+
+# --- Product Views ---
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    authentication_classes = [JWTAuthentication]
+
+    def get_permissions(self):
+        """
+        Allow:
+        - Sellers to create/update/delete products
+        - Buyers and Admins to view (GET, HEAD, OPTIONS)
+        """
+        if self.request.method in permissions.SAFE_METHODS:  # GET, HEAD, OPTIONS
+            return [IsAuthenticated(), (IsBuyer() | IsAdmin())]
+        return [IsAuthenticated(), IsSeller()]
+
 
 class ProductVariantViewSet(viewsets.ModelViewSet):
     queryset = ProductVariant.objects.all()
     serializer_class = ProductVariantSerializer
+
 
 class ProductImageViewSet(viewsets.ModelViewSet):
     queryset = ProductImage.objects.all()
